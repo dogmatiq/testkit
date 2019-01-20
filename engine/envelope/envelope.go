14fc2d3f093d0e1c6952beaee64599d6ae77f1be
James Harris <james.harris@icecave.com.au>
2019-01-20 17:37:15 +1000

    Remove children from envelopes.
    Now that the test engine uses "facts" to record what happens, there is no longer a need
    to inspect an envelope after the fact.

--- engine/envelope/envelope.go
+++ engine/envelope/envelope.go
@@ -7,9 +7,6 @@ import (
 )
 
 // Envelope is a container for a message that is handled by the test engine.
-//
-// Envelopes form a tree structure describing the causal relationships between
-// the messages they contain.
 type Envelope struct {
 	// Message is the application-defined message that the envelope represents.
 	Message dogma.Message
@@ -22,10 +19,6 @@ type Envelope struct {
 
 	// IsRoot is true if this message is at the root of an envelope tree.
 	IsRoot bool
-
-	// Children is a slice of the envelopes that were caused by this envelope's
-	// message when it was handled.
-	Children []*Envelope
 }
 
 // New constructs a new envelope containing the given message.
@@ -45,36 +38,10 @@ func New(m dogma.Message, r message.Role) *Envelope {
 func (e *Envelope) NewChild(m dogma.Message, r message.Role) *Envelope {
 	r.MustValidate()
 
-	env := &Envelope{
+	return &Envelope{
 		Message: m,
 		Type:    message.TypeOf(m),
 		Role:    r,
 		IsRoot:  false,
 	}
-
-	e.Children = append(e.Children, env)
-
-	return env
-}
-
-// Walk traverses the "envelope tree", calling fn for each of this envelope's
-// children, then their children, recursively.
-//
-// Traversal is depth-first. fn is never called with e itself, only its
-// children.
-//
-// Traversal stops when fn returns false, or all nodes have been visited.
-// It returns true if traversal visits all nodes.
-func (e *Envelope) Walk(fn func(*Envelope) bool) bool {
-	for _, env := range e.Children {
-		if !fn(env) {
-			return false
-		}
-
-		if !env.Walk(fn) {
-			return false
-		}
-	}
-
-	return true
 }
