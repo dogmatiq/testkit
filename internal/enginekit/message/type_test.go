2da52244480d9da6d667db45221ac7576531d76c
James Harris <mailjamesharris@gmail.com>
2019-01-22 07:10:24 +1000

    Use canned message fixtures in message type tests.

--- internal/enginekit/message/type_test.go
+++ internal/enginekit/message/type_test.go
@@ -12,16 +12,16 @@ import (
 var _ = Describe("type MessageType", func() {
 	Describe("func TypeOf", func() {
 		It("returns values that compare as equal for messages of the same type", func() {
-			ta := TypeOf(fixtures.MessageA{})
-			tb := TypeOf(fixtures.MessageA{})
+			ta := TypeOf(fixtures.MessageA1)
+			tb := TypeOf(fixtures.MessageA1)
 
 			Expect(ta).To(Equal(tb))
 			Expect(ta == tb).To(BeTrue()) // explicitly check the pointers for standard equality comparability
 		})
 
 		It("returns values that do not compare as equal for messages of different types", func() {
-			ta := TypeOf(fixtures.MessageA{})
-			tb := TypeOf(fixtures.MessageB{})
+			ta := TypeOf(fixtures.MessageA1)
+			tb := TypeOf(fixtures.MessageB1)
 
 			Expect(ta).NotTo(Equal(tb))
 			Expect(ta != tb).To(BeTrue()) // explicitly check the pointers for standard equality comparability
@@ -30,19 +30,16 @@ var _ = Describe("type MessageType", func() {
 
 	Describe("func ReflectType", func() {
 		It("returns the reflect.Type for the message", func() {
-			m := fixtures.MessageA{}
-			t := TypeOf(m)
+			mt := TypeOf(fixtures.MessageA1)
+			rt := reflect.TypeOf(fixtures.MessageA1)
 
-			Expect(t.ReflectType()).To(BeIdenticalTo(
-				reflect.TypeOf(m),
-			))
+			Expect(mt.ReflectType()).To(BeIdenticalTo(rt))
 		})
 	})
 
 	Describe("func String", func() {
 		It("returns the package-qualified type name and the pointer address", func() {
-			m := fixtures.MessageA{}
-			t := TypeOf(m)
+			t := TypeOf(fixtures.MessageA1)
 
 			Expect(t.String()).To(Equal(
 				"fixtures.MessageA",
@@ -50,8 +47,7 @@ var _ = Describe("type MessageType", func() {
 		})
 
 		It("supports anonymous types", func() {
-			m := struct{ inner fixtures.MessageA }{}
-			t := TypeOf(m)
+			t := TypeOf(struct{ fixtures.MessageA }{})
 
 			Expect(t.String()).To(Equal("<anonymous>"))
 		})
