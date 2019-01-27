f18d88b416efcd49c31412eb1f417eeb03feb1cb
James Harris <james.harris@icecave.com.au>
2019-01-27 12:31:04 +1000

    Add tests for fuzzy-type-comparisons.

---
+++ compare/fuzzy_test.go
@@ -0,0 +1,68 @@
+package compare_test
+
+import (
+	"reflect"
+
+	. "github.com/dogmatiq/dogmatest/compare"
+	"github.com/dogmatiq/enginekit/fixtures"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ Comparator = DefaultComparator{}
+
+var _ = Describe("func FuzzyTypeComparison", func() {
+	It("returns SameTypes when given two identical types", func() {
+		Expect(
+			FuzzyTypeComparison(
+				reflect.TypeOf(fixtures.MessageA1),
+				reflect.TypeOf(fixtures.MessageA1),
+			),
+		).To(Equal(
+			SameTypes,
+		))
+	})
+
+	It("returns SameTypes when given two unrelated types", func() {
+		Expect(
+			FuzzyTypeComparison(
+				reflect.TypeOf(fixtures.MessageA1),
+				reflect.TypeOf(fixtures.MessageB1),
+			),
+		).To(Equal(
+			UnrelatedTypes,
+		))
+	})
+
+	It("returns some intermediate value when given types that differ only by 'pointer depth'", func() {
+		a := reflect.PtrTo(reflect.TypeOf(fixtures.MessageA1))
+		b := reflect.TypeOf(fixtures.MessageA1)
+
+		sim := FuzzyTypeComparison(a, b)
+
+		Expect(sim).NotTo(Equal(SameTypes))
+		Expect(sim).NotTo(Equal(UnrelatedTypes))
+	})
+
+	It("returns the same value regardless of parameter order", func() {
+		a := reflect.PtrTo(reflect.TypeOf(fixtures.MessageA1))
+		b := reflect.TypeOf(fixtures.MessageA1)
+
+		Expect(
+			FuzzyTypeComparison(a, b),
+		).To(Equal(
+			FuzzyTypeComparison(b, a),
+		))
+	})
+
+	It("returns a higher value for more similar types", func() {
+		a := reflect.TypeOf(fixtures.MessageA1)
+		b := reflect.PtrTo(a)
+		c := reflect.PtrTo(b)
+
+		simA := FuzzyTypeComparison(a, b)
+		simB := FuzzyTypeComparison(a, c)
+
+		Expect(simA).To(BeNumerically(">", simB))
+	})
+})
