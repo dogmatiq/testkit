484a08e2ec042efab21eac99ad0fad3e2411c96c
James Harris <mailjamesharris@gmail.com>
2019-01-31 09:25:33 +1000

    Move handling related facts into `dispatch.go`.

--- engine/fact/dispatch.go
+++ engine/fact/dispatch.go
@@ -30,3 +30,28 @@ type MessageDispatchCompleted struct {
 	Error           error
 	EnabledHandlers map[handler.Type]bool
 }
+
+// MessageHandlingBegun indicates that a message is about to be handled by a
+// specific handler.
+type MessageHandlingBegun struct {
+	HandlerName string
+	HandlerType handler.Type
+	Envelope    *envelope.Envelope
+}
+
+// MessageHandlingCompleted indicates that a message has been handled by a
+// specific handler, either successfully or unsucessfully.
+type MessageHandlingCompleted struct {
+	HandlerName string
+	HandlerType handler.Type
+	Envelope    *envelope.Envelope
+	Error       error
+}
+
+// MessageHandlingSkipped indicates that a message has been not been handled by
+// a specific handler, because handlers of that type are disabled.
+type MessageHandlingSkipped struct {
+	HandlerName string
+	HandlerType handler.Type
+	Envelope    *envelope.Envelope
+}
