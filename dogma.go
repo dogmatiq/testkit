6dde3eb6ce1dee3b934f6fb5b42099c1229ee107
James Harris <james.harris@icecave.com.au>
2019-02-03 08:33:31 +1000

    Add a note about why we don't use `t.Helper()`.

--- dogma.go
+++ dogma.go
@@ -15,3 +15,9 @@ func logf(t T, f string, args ...interface{}) { t.Logf(f, args...) }
 // Note also, that all of the logging calls within this file are made on a
 // single-digit line-number, ensuring that all output is aligned and as short as
 // possible.
+//
+// Go *does* include the testing.T.Helper() function, which marks the
+// calling-function as a "helper" so that it does not get considered as the
+// source of logs. Unfortunately, this only marks a single stack frame as a
+// "helper", and so it does not work with the enginekit Logger, which is not
+// aware of that it's being used within tests.
