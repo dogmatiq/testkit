ffa60dacde7606b79577139f7eb18fb959523d9b
James Harris <james.harris@icecave.com.au>
2019-02-03 08:14:21 +1000

    Improve documentation around the `dogma.go` log-proxy file.

--- dogma.go
+++ dogma.go
@@ -1,16 +1,17 @@
 package dogmatest
 
-// This file (dogma.go) is used to write log messages to the *testing.T struct
-// (via the T interface), so that the test output more clearly indicates that
-// the log message originated within Dogma's tooling, as the Go test framework
-// includes the filename in log messages.
+func log(t T, args ...interface{})            { t.Log(args...) }
+func logf(t T, f string, args ...interface{}) { t.Logf(f, args...) }
 
-// log logs a message during a test.
-func log(t T, args ...interface{}) {
-	t.Log(args...)
-}
-
-// logf logs a formatted message during a test.
-func logf(t T, f string, args ...interface{}) {
-	t.Logf(f, args...)
-}
+// ABOUT THIS FILE (dogma.go)
+//
+// Go's built-in test framework includes the filename and line number of any
+// calls that are made to the testing.T.Log() function and its variants.
+//
+// In an effort to make it clearer that the log output produced by this package
+// originates within Dogma's tooling we perform all test logging via the
+// functions above.
+//
+// Note also, that all of the logging calls within this file are made on a
+// single-digit line-number, ensuring that all output is aligned and as short as
+// possible.
