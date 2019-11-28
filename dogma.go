472d6cb2b63180fc3e223a97e400210f784e8b6c
James Harris <mailjamesharris@gmail.com>
2019-11-28 19:26:38 +1000

    Update explanation of dogma.go.

--- dogma.go
+++ dogma.go
@@ -8,7 +8,7 @@ func log(t T, args ...interface{}) { t.Log(args...) }
 // calls that are made to the testing.T.Log() function and its variants.
 //
 // In an effort to make it clearer that the log output produced by this package
-// originates within Dogma's tooling we perform all test logging via the
+// originates within Dogma's tooling we perform all engine logging via the
 // functions above.
 //
 // Note also, that all of the logging calls within this file are made on a
@@ -18,5 +18,5 @@ func log(t T, args ...interface{}) { t.Log(args...) }
 // Go *does* include the testing.T.Helper() function, which marks the
 // calling-function as a "helper" so that it does not get considered as the
 // source of logs. Unfortunately, this only marks a single stack frame as a
-// "helper", and so it does not work with the enginekit Logger, which is not
-// aware of that it's being used within tests.
+// "helper", and so it does not work for log messages generated deeply within
+// the engine.
