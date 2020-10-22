2dc6a594091e469eed7baa7e1d7fbbaf20553ae7
James Harris <mailjamesharris@gmail.com>
2020-10-22 20:11:11 +1000

    Correct misspelling `TypeAdvance` -> `TypeAdvancer`.

--- time.go
+++ time.go
@@ -9,14 +9,14 @@ import (
 // advanced to during a call to Test.AdvanceTime().
 type TimeAdvancer func(before time.Time) (after time.Time, description string)
 
-// ToTime returns a TimeAdvance that advances the engine time to a specific time.
+// ToTime returns a TimeAdvancer that advances the engine time to a specific time.
 func ToTime(t time.Time) TimeAdvancer {
 	return func(time.Time) (time.Time, string) {
 		return t, fmt.Sprintf("ADVANCING TIME TO %s", t.Format(time.RFC3339))
 	}
 }
 
-// ByDuration returns a TimeAdvance that advances the engine time by a fixed duration.
+// ByDuration returns a TimeAdvancer that advances the engine time by a fixed duration.
 func ByDuration(d time.Duration) TimeAdvancer {
 	return func(now time.Time) (time.Time, string) {
 		return now.Add(d), fmt.Sprintf("ADVANCING TIME BY %s", d)
