cbbac9dce83b0952e5456bfdcde24c427130d197
James Harris <mailjamesharris@gmail.com>
2020-11-14 17:44:42 +1000

    Add the `Action` interface.

---
+++ action.go
@@ -0,0 +1,26 @@
+package testkit
+
+import (
+	"github.com/dogmatiq/testkit/assert"
+	"github.com/dogmatiq/testkit/engine"
+)
+
+// Action is an interface for any action that can be performed within a test.
+//
+// Actions always attempt to cause some state change within the engine or
+// application.
+type Action interface {
+	// Heading returns a human-readable description of the action, used as a
+	// heading within the test report.
+	//
+	// Any engine activity as a result of this action is logged beneath this
+	// heading.
+	Heading() string
+
+	// Apply performs the action within the context of a specific test.
+	Apply(
+		t *Test,
+		a assert.Assertion,
+		options []engine.OperationOption,
+	)
+}
