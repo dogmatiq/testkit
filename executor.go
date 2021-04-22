8e7eef32178670b817908b851c663dbfa6bfe4b3
James Harris <mailjamesharris@gmail.com>
2021-04-23 09:07:34 +1000

    Add `InterceptCommandExecutor()` option

--- executor.go
+++ executor.go
@@ -13,8 +13,9 @@ import (
 //
 // An instance can be obtained at any time by calling Test.CommandExecutor().
 type commandExecutor struct {
-	m    sync.RWMutex
-	next engine.CommandExecutor
+	m           sync.RWMutex
+	next        engine.CommandExecutor
+	interceptor CommandExecutorInterceptor
 }
 
 // ExecuteCommand executes the command message m.
@@ -22,16 +23,20 @@ func (c *commandExecutor) ExecuteCommand(ctx context.Context, m dogma.Message) e
 	c.m.RLock()
 	defer c.m.RUnlock()
 
-	if c.next.Engine != nil {
-		return c.next.ExecuteCommand(ctx, m)
+	if c.next.Engine == nil {
+		panic("ExecuteCommand(): can not be called outside of a test action")
 	}
 
-	panic("ExecuteCommand(): can not be called outside of a test action")
+	if c.interceptor != nil {
+		return c.interceptor(ctx, m, c.next)
+	}
+
+	return c.next.ExecuteCommand(ctx, m)
 }
 
 // bind sets the engine and options that should be used to execute commands.
 //
-// Binding to an engine allowed the executor to be used. A call to unbind() must
+// Binding to an engine allows the executor to be used. A call to unbind() must
 // be made when the executor should not longer be used.
 func (c *commandExecutor) bind(e *engine.Engine, options []engine.OperationOption) {
 	c.m.Lock()
@@ -50,4 +55,46 @@ func (c *commandExecutor) unbind() {
 
 	c.next.Engine = nil
 	c.next.Options = nil
+}
+
+// intercept installs an interceptor function that is invoked whenever
+// ExecuteCommand() is called.
+//
+// If fn is nil the interceptor is removed.
+//
+// It returns the previous interceptor, if any.
+func (c *commandExecutor) intercept(fn CommandExecutorInterceptor) CommandExecutorInterceptor {
+	c.m.Lock()
+	defer c.m.Unlock()
+
+	prev := c.interceptor
+	c.interceptor = fn
+
+	return prev
+}
+
+// CommandExecutorInterceptor is used by the InterceptCommandExecutor() option
+// to specify custom behavior for the dogma.CommandExecutor returned by
+// Test.CommandExecutor().
+//
+// m is the command being executed.
+//
+// e can be used to execute the command as it would be executed without this
+// interceptor installed.
+type CommandExecutorInterceptor func(
+	ctx context.Context,
+	m dogma.Message,
+	e dogma.CommandExecutor,
+) error
+
+// InterceptCommandExecutor returns an option that causes fn to be called
+// whenever a command is executed via the dogma.CommandExecutor returned by
+// Test.CommandExecutor().
+//
+// Intercepting calls to the command executor allows the user to simulate
+// failures (or any other behavior) in the command executor.
+func InterceptCommandExecutor(fn CommandExecutorInterceptor) TestOption {
+	return testOptionFunc(func(t *Test) {
+		t.executor.intercept(fn)
+	})
 }
