7aedc4b00104308f11808e3816194ec266946a7f
James Harris <mailjamesharris@gmail.com>
2021-04-28 10:58:42 +1000

    Remove `report.Diff()`, and remove error return value from `WriteDiff()`.

--- internal/report/diff.go
+++ internal/report/diff.go
@@ -2,16 +2,13 @@ package report
 
 import (
 	"io"
-	"strings"
 
 	"github.com/dogmatiq/iago/must"
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
 // WriteDiff renders a human-readable diff of two strings.
-func WriteDiff(w io.Writer, a, b string) (n int, err error) {
-	defer must.Recover(&err)
-
+func WriteDiff(w io.Writer, a, b string) {
 	d := diffmatchpatch.New()
 
 	for _, diff := range d.DiffMain(a, b, false) {
@@ -19,24 +16,15 @@ func WriteDiff(w io.Writer, a, b string) (n int, err error) {
 
 		switch diff.Type {
 		case diffmatchpatch.DiffInsert:
-			n += must.WriteString(w, "{+")
-			n += must.WriteString(w, text)
-			n += must.WriteString(w, "+}")
+			must.WriteString(w, "{+")
+			must.WriteString(w, text)
+			must.WriteString(w, "+}")
 		case diffmatchpatch.DiffDelete:
-			n += must.WriteString(w, "[-")
-			n += must.WriteString(w, text)
-			n += must.WriteString(w, "-]")
+			must.WriteString(w, "[-")
+			must.WriteString(w, text)
+			must.WriteString(w, "-]")
 		case diffmatchpatch.DiffEqual:
-			n += must.WriteString(w, text)
+			must.WriteString(w, text)
 		}
 	}
-
-	return
-}
-
-// Diff returns a human-readable diff of two strings.
-func Diff(a, b string) string {
-	var w strings.Builder
-	must.Must(WriteDiff(&w, a, b))
-	return w.String()
 }
--- internal/report/diff_test.go
+++ internal/report/diff_test.go
@@ -1,10 +1,8 @@
 package report_test
 
 import (
-	"io"
+	"strings"
 
-	"github.com/dogmatiq/iago/iotest"
-	"github.com/dogmatiq/iago/must"
 	. "github.com/dogmatiq/testkit/internal/report"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,31 +10,16 @@ import (
 
 var _ = Describe("func WriteDiff()", func() {
 	It("produces a word-diff of the input", func() {
-		iotest.TestWrite(
-			GinkgoT(),
-			func(w io.Writer) int {
-				return must.Must(
-					WriteDiff(
-						w,
-						"foo bar baz",
-						"foo qux baz",
-					),
-				)
-			},
-			"foo [-bar-]{+qux+} baz",
-		)
-	})
-})
+		var w strings.Builder
 
-var _ = Describe("func Diff()", func() {
-	It("produces a word-diff of the input", func() {
-		Expect(
-			Diff(
-				"foo bar baz",
-				"foo qux baz",
-			),
-		).To(Equal(
-			"foo [-bar-]{+qux+} baz",
-		))
+		WriteDiff(
+			&w,
+			"foo bar baz",
+			"foo qux baz",
+		)
+
+		Expect(w.String()).To(
+			Equal("foo [-bar-]{+qux+} baz"),
+		)
 	})
 })
