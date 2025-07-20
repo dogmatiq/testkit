d3618c23367a5c1fb5b1b047bc679b50303193d4
James Harris <contact@jamesharris.id.au>
2025-07-21 07:30:20 +1000

    Update projection controller to use new OCC system.

--- engine/internal/projection/scope.go
+++ engine/internal/projection/scope.go
@@ -8,21 +8,34 @@ import (
 	"github.com/dogmatiq/testkit/fact"
 )
 
-// scope is an implementation of dogma.ProjectionEventScope and
-// dogma.ProjectionCompactScope.
+// scope is an implementation of [dogma.ProjectionEventScope] and
+// [dogma.ProjectionCompactScope].
 type scope struct {
-	config   *config.Projection
-	observer fact.Observer
-	event    *envelope.Envelope // nil if compacting
-	now      time.Time
+	config     *config.Projection
+	observer   fact.Observer
+	event      *envelope.Envelope // nil if compacting
+	checkpoint uint64
+	now        time.Time
 }
 
 func (s *scope) RecordedAt() time.Time {
 	return s.event.CreatedAt
 }
 
-func (s *scope) IsPrimaryDelivery() bool {
-	return true
+func (s *scope) StreamID() string {
+	return s.event.EventStreamID
+}
+
+func (s *scope) Offset() uint64 {
+	return s.event.EventStreamOffset
+}
+
+func (s *scope) CheckpointOffset() uint64 {
+	return s.checkpoint
+}
+
+func (s *scope) Now() time.Time {
+	return s.now
 }
 
 func (s *scope) Log(f string, v ...any) {
@@ -32,8 +45,4 @@ func (s *scope) Log(f string, v ...any) {
 		LogFormat:    f,
 		LogArguments: v,
 	})
-}
-
-func (s *scope) Now() time.Time {
-	return s.now
 }
